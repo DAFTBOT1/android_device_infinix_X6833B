9af6e0af46bca352647539224812aa8780748a19
Mr.Belt <82068460+DAFTBOT1@users.noreply.github.com>
2026-07-20 12:17:26 +0700

    Modify copyright and product name in makefile
    Updated copyright holder and product name to reflect lineage.

--- lineage_X6833B.mk
+++ lineage_X6833B.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2023 The Infinity-X Project
+# Copyright (C) 2023 The Infinity-X Project and Linus Torvalds
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,18 +11,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/infinix/X6833B/device.mk)
 
-# Inherit some common Infinity stuff.
-$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Infinity-X Specific Flags
-INFINITY_BUILD_TYPE := OFFICIAL
-INFINITY_MAINTAINER := "akif"
-TARGET_BOOT_ANIMATION_RES := 1080
-WITH_GAPPS := true
-TARGET_SUPPORTS_BLUR := true
 
 BOARD_VENDOR := Infinix
-PRODUCT_NAME := infinity_X6833B
+PRODUCT_NAME := lineage_X6833B
 PRODUCT_DEVICE := X6833B
 PRODUCT_MANUFACTURER := INFINIX
 PRODUCT_BRAND := Infinix
