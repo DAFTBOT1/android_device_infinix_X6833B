461c0e3412010e906957e7ebedd65e2c94cdff89
Mr.Belt <82068460+DAFTBOT1@users.noreply.github.com>
2026-07-31 08:01:40 +0700

    Update and rename lineage_X6833B.mk to X6833B.mk

--- X6833B.mk
+++ X6833B.mk
@@ -11,12 +11,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/infinix/X6833B/device.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common luminedroid stuff.
+$(call inherit-product, vendor/lumine/config/common_full_phone.mk)
 
 
 BOARD_VENDOR := Infinix
-PRODUCT_NAME := lineage_X6833B
+PRODUCT_NAME := X6833B
 PRODUCT_DEVICE := X6833B
 PRODUCT_MANUFACTURER := INFINIX
 PRODUCT_BRAND := Infinix
@@ -35,5 +35,5 @@ LINEAGE_VERSION_APPEND_TIME_OF_DAY := true
 PERF_ANIM_OVERRIDE := true
 
 # GMS Flagg if needed (remove #)
-# WITH_GMS := true/false
+WITH_GMS := true
 
